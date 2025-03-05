dc4c41c96f34f8caced7588459bc2318ac6decca
sym242 <ss.syusyoku6125@gmail.com>
2025-03-05 10:05:29 +0900

    feat:DBのデータを取得する

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,6 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/todo', function () {
-    echo 'Hello World!';
-});
+Route::get('/todo', 'TodoController@index');
+
+// Route::get('/todo', function () {
+//     echo 'Hello World!';
+// });
+
