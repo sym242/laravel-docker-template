939384dadf67d168c5edd6b73b373de37039a202
sym242 <ss.syusyoku6125@gmail.com>
2025-04-02 11:34:07 +0900

    feat:ToDo詳細表示機能の実装

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,15 +15,14 @@ Route::get('/', function () {
     return view('   welcome');
 });
 
-Route::get('/todo', 'TodoController@index');
 
 // Route::get('/todo/create', 'TodoController@create');
 
 Route::get('/todo/create', 'TodoController@create')->name('todo.create');
-// Route::get('/todo', function () {
-//     echo 'Hello World!';
-// });
 
 Route::post('/todo', 'TodoController@store')->name('todo.store');
+
 Route::get('/todo', 'TodoController@index')->name('todo.index');
+
+Route::get('/todo/{id}', 'TodoController@show')->name('todo.show');
 
